e33e469c8ad0a4233a14a29a7001daad07901d08
infoplasma <lorenzoamante@infoplasma.com>
2019-12-17 23:14:36 +0100

    bug fix: configuration reset, OK.

--- lun_provisioner.py
+++ lun_provisioner.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import npyscreen as nps
-from writers import write_output, write_lun_provision_yaml
 from yaml import safe_load, safe_dump
 from jinja2 import Environment, FileSystemLoader
 
@@ -13,36 +12,34 @@ def stop_editability(self):
     self.REPLICA.editable = False
     self.LUN_TYPE.editable = False
 
+
 def enable_editablity(self):
     self.HOSTNAMES.editable = True
     self.TIER.editable = True
     self.REPLICA.editable = True
     self.LUN_TYPE.editable = True
 
+
 def reset_values(self):
     with open('config/config_email.yaml') as yaml_data:
         cfg = safe_load(yaml_data)
+    self.HOSTNAMES.value = cfg['HOSTNAMES']
+    self.TIER.value = 0
+    self.REPLICA.value = 0
+    self.LUN_TYPE.value = 0
+    self.LUN_GB.value = cfg['LUN_GB']
+    self.LUN_QTY.value = cfg["LUN_QTY"]
+    self.LUN_GRID.values = [[]],
 
-    self.HOSTNAMES.value=cfg['HOSTNAMES']
-    self.TIER.value=0
-    self.REPLICA.value=0
-    self.LUN_TYPE.value=0
-    self.LUN_GB.value=cfg['LUN_GB']
-    self.LUN_QTY.value=cfg["LUN_QTY"]
-    self.LUN_GRID.values=[[]],
 
-
-class lun_provisionerForm(nps.ActionFormV2):
-
+class LunProvisionerForm(nps.ActionFormV2):
     def create(self):
-
         with open('config/config_email.yaml') as yaml_data:
             cfg = safe_load(yaml_data)
-
-        self.HOSTNAMES = self.add(nps.TitleText, name="HOSTNAMES:", value=cfg['HOSTNAMES'], color='STANDOUT')
-
+        self.HOSTNAMES = self.add(nps.TitleText, name="HOSTNAMES:", value=cfg['HOSTNAMES'],
+                                  color='STANDOUT')
         self.TIER = self.add(nps.TitleSelectOne, max_height=3, name="TIER:",
-                                  values=cfg['TIER'], value=0, scroll_exit=True, rely=4)
+                             values=cfg['TIER'], value=0, scroll_exit=True, rely=4)
         self.REPLICA = self.add(nps.TitleSelectOne, max_height=2, max_width=30, name="REPLICATED:",
                                 values=["YES", "NO"], value=0, scroll_exit=True, rely=8)
         self.LUN_TYPE = self.add(nps.TitleSelectOne, max_height=3, max_width=30, name="LUN TYPE:",
@@ -52,42 +49,40 @@ class lun_provisionerForm(nps.ActionFormV2):
         self.LUN_QTY = self.add(nps.TitleText, name="QTY:", value=cfg["LUN_QTY"],
                                 max_width=34, relx=26, rely=12)
         self.LUN_GRID = self.add(nps.GridColTitles,
-                              values=[[]],
-                              col_titles=["HOSTNAME", "SIZE [GB]", "QUANTITY", "TIER", "PREFIX", "REPLICA"],
-                              columns=5,
-                              column_width=12,
-                              right_align=True,
-                              select_whole_line=False,
-                              always_show_cursor=False,
-                              rely=17,
-                              editable=False)
+                                 values=[[]],
+                                 col_titles=["HOSTNAME", "SIZE [GB]", "QUANTITY", "TIER", "PREFIX", "REPLICA"],
+                                 columns=5,
+                                 column_width=12,
+                                 right_align=True,
+                                 select_whole_line=False,
+                                 always_show_cursor=False,
+                                 rely=17,
+                                 editable=False)
 
     def on_ok(self):
         stop_editability(self)
 
-        TIER_SEL = self.TIER
-        TIER = TIER_SEL.values[TIER_SEL.value[0]]
-        LDEV_PREFIX_SEL = self.LUN_TYPE
-        LDEV_PREFIX = LDEV_PREFIX_SEL.values[LDEV_PREFIX_SEL.value[0]]
-        LDEV_PREFIX = LDEV_PREFIX.rstrip("_")
-        REPLICA_SEL = self.REPLICA
-        GAD = REPLICA_SEL.values[REPLICA_SEL.value[0]]
+        TIER = self.TIER.values[self.TIER.value[0]]
+        LDEV_PREFIX = self.LUN_TYPE.values[self.LUN_TYPE.value[0]]
+        GAD = self.REPLICA.values[self.REPLICA.value[0]]
         IS_GAD = True if GAD == 'YES' else False
-
-        self.LUN_GRID.values.append([self.HOSTNAMES.value, self.LUN_GB.value, self.LUN_QTY.value, TIER, LDEV_PREFIX, IS_GAD])
+        self.LUN_GRID.values.append(
+            [self.HOSTNAMES.value, self.LUN_GB.value, self.LUN_QTY.value, TIER, LDEV_PREFIX, IS_GAD])
 
         self.parentApp.switchForm("LUN PROVISIONER")
 
     def on_cancel(self):
         nps.notify_wait("CONFIGURATION REVIEW.")
+        self.parentApp.getForm("CONFIGURATION REVIEW").wgt.values = self.LUN_GRID.values
         self.parentApp.switchForm("CONFIGURATION REVIEW")
 
 
 class configurationReviewForm(nps.ActionFormV2):
     def create(self):
-        self.mem = self.parentApp.getForm("LUN PROVISIONER")
+        #self.mem = self.parentApp.getForm("LUN PROVISIONER").LUN_GRID.values
 
-        self.wgt = self.add(nps.GridColTitles, values=self.mem.LUN_GRID.values,
+        self.wgt = self.add(nps.GridColTitles,
+                            values=self.parentApp.getForm("LUN PROVISIONER").LUN_GRID.values,
                             col_titles=["HOSTNAME", "SIZE [GB]", "QUANTITY", "TIER", "PREFIX", "REPLICA"],
                             columns=6,
                             column_width=12,
@@ -97,50 +92,37 @@ class configurationReviewForm(nps.ActionFormV2):
                             editable=False)
 
     def on_ok(self):
-
-        nps.notify_confirm(str(self.wgt.values))
-
-        data_list = self.wgt.values
-
-        j = {}
+        data_list = self.parentApp.getForm("LUN PROVISIONER").LUN_GRID.values
+        nps.notify_ok_cancel(str(data_list))
         data = {'hostname': data_list[1][0],
                 'tier': data_list[1][3],
                 'prefix': data_list[1][4],
                 'replica': data_list[1][5],
                 'devices': [{'size_gb': i[1], 'qty': i[2]} for i in data_list if i]}
-
-        with open("output_email.txt", "w", encoding='utf-8') as file:
-            for line in self.wgt.values:
-                file.writelines(str(line) + '\n')
-
         with open("vars/output_params.yaml", "w", encoding='utf-8') as handle:
             safe_dump(data, handle, allow_unicode=True)
-
         nps.notify_wait("INFO: WRITING CONFIGURATION FILE.")
-
-
         with open("vars/output_params.yaml", "r") as handle:
             devs = safe_load(handle)
-
         j2_env = Environment(loader=FileSystemLoader("."), trim_blocks=True, autoescape=True)
-
         template = j2_env.get_template("templos/email_templo.j2")
-
         config = template.render(data=devs)
         with open("output.txt", "w") as output:
             output.write(config)
 
-
-        enable_editablity(self.parentApp.getForm("LUN PROVISIONER"))
+        #enable_editablity(self.parentApp.getForm("LUN PROVISIONER"))
         self.parentApp.switchForm("MAIN")
 
     def on_cancel(self):
+        """
         self.wgt.values = [[]]
         self.parentApp.switchForm("LUN PROVISIONER")
         reset_values(self.parentApp.getForm("LUN PROVISIONER"))
-        enable_editablity(self.parentApp.getForm("LUN PROVISIONER"))
+        #enable_editablity(self.parentApp.getForm("LUN PROVISIONER"))
         nps.notify_wait("INFO: ERASING CONFIGURATION.")
-
         self.mem.LUN_GRID.values = [[]]
+        """
+        nps.notify_wait("INFO: ERASING CONFIGURATION.")
+        self.parentApp.getForm("LUN PROVISIONER").LUN_GRID.values = [[]]
+        self.wgt.values = [[]]
         self.parentApp.switchForm("MAIN")
-
--- service_selector.py
+++ service_selector.py
@@ -4,7 +4,7 @@
 print("*** INFO: LOADING PROGRAM, PLEASE BE PATIENT. ***")
 
 import npyscreen as nps
-from lun_provisioner import lun_provisionerForm, configurationReviewForm
+from lun_provisioner import LunProvisionerForm, configurationReviewForm
 from lun_decommissioner import lun_decommissionerForm
 from host_provisioner import host_provisionerForm
 from host_decommissioner import host_decommissionerForm
@@ -41,7 +41,7 @@ class MyApplication(nps.NPSAppManaged):
     def onStart(self):
         nps.setTheme(nps.Themes.ElegantTheme)
         self.addForm('MAIN', myEntryForm, name='STORAGE SERVICE MANAGER')
-        self.addForm('LUN PROVISIONER', lun_provisionerForm, name="LUN PROVISIONER")
+        self.addForm('LUN PROVISIONER', LunProvisionerForm, name="LUN PROVISIONER")
         self.addForm('LUN DECOMMISSIONER', lun_decommissionerForm, name="LUN DECOMMISSIONER")
         self.addForm('HOST PROVISIONER', host_provisionerForm, name="HOST PROVISIONER")
         self.addForm('HOST DECOMMISSIONER', host_decommissionerForm, name="HOST DECOMMISSIONER")
